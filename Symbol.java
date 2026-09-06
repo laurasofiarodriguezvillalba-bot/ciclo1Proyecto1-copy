8aacfb898c63642371bbf08df184217f21163e26
Luisa-quintero <luisa.quintero-m@mail.escuelaing.edu.co>
2026-09-06 18:21:41 -0500

    Refactor Symbol class by removing circle functionality
    Removed comments and circle-related functionality from the Symbol class.

--- Symbol.java
+++ Symbol.java
@@ -1,42 +1,13 @@
-/**
- * Represents a symbol of the slot machine.
- * Each symbol is identified by a CSS color.
- */
 public class Symbol {
     private String color;
-    private Circle circle;
 
-    /**
-     * Creates a symbol with the specified color.
-     *
-     * @param color the CSS color of the symbol
-     */
+    //Crea un simbolo con un color
     public Symbol(String color) {
         this.color = color;
-        circle = new Circle();
-        circle.changeColor(color);
     }
 
-    /**
-     * Returns the color of the symbol.
-     *
-     * @return the color of the symbol
-     */
+    //Retorna el color del simbolo
     public String getColor() {
         return color;
-    }
-
-    /**
-     * Makes the symbol visible.
-     */
-    public void makeVisible() {
-        circle.makeVisible();
-    }
-
-    /**
-     * Makes the symbol invisible.
-     */
-    public void makeInvisible() {
-        circle.makeInvisible();
     }
 }
