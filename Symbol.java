6f00838073054ae0f04f2d8952b68df50fbaf7f7
Luisa-quintero <luisa.quintero-m@mail.escuelaing.edu.co>
2026-09-06 19:49:02 -0500

    Update Symbol.java

--- Symbol.java
+++ Symbol.java
@@ -1,11 +1,9 @@
 public class Symbol {
     private String color;
-
     //Create a symbol with a color
     public Symbol(String color) {
         this.color = color;
     }
-
     //Returns the symbol color
     public String getColor() {
         return color;
